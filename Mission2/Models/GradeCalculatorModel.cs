0e08e8114c519c771fd68086feee5f803e3a0d26
sweetdaniela23 <90408575+sweetdaniela23@users.noreply.github.com>
2022-01-19 20:59:03 -0700

    with validation

--- Mission2/Models/GradeCalculatorModel.cs
+++ Mission2/Models/GradeCalculatorModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace Mission2.Models
 {
     public class GradeCalculatorModel
     {
+        [Range(0, 100)]
         public sbyte assignments { get; set; }
+        [Range(0, 100)]
         public sbyte projects { get; set; }
+        [Range(0, 100)]
         public sbyte quizzes { get; set; }
+        [Range(0, 100)]
         public sbyte exams { get; set; }
+        [Range(0, 100)]
         public sbyte intex { get; set; }
 
     }
